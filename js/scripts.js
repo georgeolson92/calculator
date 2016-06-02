57c5402ae7b5bc0dad3c229ac5ee0d5b7dfe8d04
George Olson and Maarja Laineste <george@>
2016-06-02 14:04:02 -0700

    change user interface logic to include branching, adjust index.html to only have one form with multiple operator options

--- js/scripts.js
+++ js/scripts.js
@@ -17,61 +17,22 @@ var divide = function(number1, number2) {
 // Everything below this line is user interface (or front-end) logic:
 
 $(document).ready(function() {
-	$("form#add").submit(function(event) {
+	$("form#calculator").submit(function() {
 		event.preventDefault();
-		var number1 = parseInt($("#add1").val());
-		var number2 = parseInt($("#add2").val());
-		var result = add(number1, number2);
+		var number1 = parseInt($("#input1").val());
+		var number2 = parseInt($("#input2").val());
+		var operator = $("input:radio[name=operator]:checked").val();
+		var result;
+		if (operator === "add") {
+			result = add(number1, number2);
+		} else if (operator === "subtract") {
+			result = subtract(number1, number2);
+		} else if (operator === "multiply") {
+			result = multiply(number1, number2);
+		} else if (operator === "divide") {
+			result = divide(number1, number2);
+		}
 		$("#output").text(result);
 	});
-	$("form#subtract").submit(function(event) {
-		event.preventDefault();
-		var number1 = parseInt($("#subtract1").val());
-		var number2 = parseInt($("#subtract2").val());
-		var result = subtract(number1, number2);
-		$("#output").text(result);
-	});
-	$("form#multiply").submit(function(event) {
-		event.preventDefault();
-		var number1 = parseInt($("#multiply1").val());
-		var number2 = parseInt($("#multiply2").val());
-		var result = multiply(number1, number2);
-		$("#output").text(result);
-	});
-	$("form#divide").submit(function(event) {
-		event.preventDefault();
-		var number1 = parseInt($("#divide1").val());
-		var number2 = parseInt($("#divide2").val());
-		var result = divide(number1, number2);
-		$("#output").text(result);
-	});
+
 });
-
-/*var weight = parseInt(prompt("What is your weight (in pounds):"));
-var height = parseInt(prompt("What is your height (in inches):"));
-
-var weightMetric = function(weight) {
-	return weight * .45;
-};
-
-var heightMetric = function(height) {
-	return height * .025;
-};
-
-var convertedWeight = weightMetric(weight);
-
-var convertedHeight = heightMetric(height);
-
-var heightSquared = function(convertedHeight) {
-	return convertedHeight * convertedHeight;
-};
-
-var heightResult = heightSquared(convertedHeight);
-
-var bmi = function(convertedWeight, heightResult) {
-	return convertedWeight / heightResult;
-};
-
-var resultBmi = bmi(convertedWeight, heightResult);
-
-alert("your BMI is " + resultBmi);*/
